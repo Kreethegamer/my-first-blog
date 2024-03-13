c7c13715187fb84e375c08c7a9b7eb06306faf2b
Kreethegamer <132790185+Kreethegamer@users.noreply.github.com>
2024-03-12 23:04:44 -0700

    Add files via upload

---
+++ personal/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+#def home_screen_view(request):
+	
+	#context = {}
+
+	#list_of_values = []
+	#list_of_values.append("first entry")
+	#context['list_of_values'] = list_of_values
+
+	#questions = Questions.objects.all()
+	#context['questions'] = questions
+
+
+	#return render(request, "personal/home.html", context )
+
